a2bfe8c9b29b6c756bbf48059a835f7693606ccd
LeoRickli <106999054+leorickli@users.noreply.github.com>
2023-12-31 13:57:44 -0300

    Add files via upload

---
+++ mongostat.py
@@ -0,0 +1,22 @@
+from concurrent.futures import ThreadPoolExecutor
+from pymongo import MongoClient
+import random
+
+# Function to insert a document
+def insert_document(client, i):
+    db = client.sampleDatabase
+    collection = db.LM_Number1
+    document = {"_id": i, "value": random.randint(1, 1000)}
+    collection.insert_one(document)
+
+# Main script
+def main():
+    client = MongoClient('mongodb://localhost:27017/')
+
+    # Using ThreadPoolExecutor to insert documents in parallel
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        for i in range(50000):
+            executor.submit(insert_document, client, i)
+
+if __name__ == "__main__":
+    main()
